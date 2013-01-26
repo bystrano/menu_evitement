a8a02a8d1defa9952386129bc8de3ee8025f14a8
bystrano <bystrano@gmx.ch>
2013-01-26 13:12:16 +0100

    commit initial - genere par la fabrique

---
+++ lang/menu_evitement_fr.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+ 
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'menu_evitement_titre' => 'Menu d\'évitement',
+
+	// C
+	'cfg_exemple' => 'Exemple',
+	'cfg_exemple_explication' => 'Explication de cet exemple',
+	'cfg_titre_parametrages' => 'Paramétrages',
+
+	// T
+	'titre_page_configurer_menu_evitement' => 'Menu d\'évitement',
+);
+
+?>
---
+++ lang/paquet-menu_evitement_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'menu_evitement_description' => 'Améliore l\'accessibilité de votre site en insérant un menu d\'évitement pour la navigation au clavier.',
+	'menu_evitement_nom' => 'Menu d\'évitement',
+	'menu_evitement_slogan' => 'Ajouter un menu d\'évitement très facilement',
+);
+
+?>

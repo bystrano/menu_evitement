64164351c1f2ab2e47c427c944be11b1f4ca9b5f
bystrano <bystrano@gmx.ch>
2013-01-27 17:38:37 +0100

    remplace les caracteres accentues des fichier de langue par du html

--- lang/paquet-menu_evitement_fr.php
+++ lang/paquet-menu_evitement_fr.php
@@ -6,9 +6,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
-	'menu_evitement_description' => 'Améliore l\'accessibilité de votre site en insérant un menu d\'évitement pour la navigation au clavier.',
-	'menu_evitement_nom' => 'Menu d\'évitement',
-	'menu_evitement_slogan' => 'Ajouter un menu d\'évitement très facilement',
+	'menu_evitement_description' => 'Am&eacute;liore l\'accessibilit&eacute; de votre site en ins&eacute;rant un menu d\'&eacute;vitement pour la navigation au clavier.',
+	'menu_evitement_nom' => 'Menu d\'&eacute;vitement',
+	'menu_evitement_slogan' => 'Ajouter un menu d\'&eacute;vitement tr&egrave;s facilement',
 );
 
 ?>
